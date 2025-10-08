f650fd15ba70a556affd3f585837dc0c1da0fa4b
Azat Saaryan <azat.saaryan2022@gmail.com>
2025-10-08 14:06:11 +0400

    Add user registration functionality

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body(new ValidationPipe()) userData: CreateUserDto) {
+    return this.authService.registerUser(userData.email, userData.password);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async registerUser(
+    email: string,
+    password: string,
+  ): Promise<Omit<User, 'passwordHash'>> {
+    try {
+      const existingUser = await this.prisma.user.findUnique({
+        where: { email },
+      });
+
+      if (existingUser) throw new ConflictException('User already exists');
+
+      const salt = await bcrypt.genSalt(10);
+      const passwordHash = await bcrypt.hash(password, salt);
+
+      const newUser = await this.prisma.user.create({
+        data: { email, passwordHash },
+        select: { id: true, email: true, createdAt: true, updatedAt: true },
+      });
+
+      return newUser;
+    } catch (error) {
+      console.error('Error registering user:', error);
+      throw error;
+    }
+  }
+}
