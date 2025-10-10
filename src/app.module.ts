74232f700f7d2d4126509ab9734b00b218638b79
Azat Saaryan <azat.saaryan2022@gmail.com>
2025-10-10 20:46:03 +0400

    Generated deposits res

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { RedisModule } from './redis/redis.module';
 import { AppJwtModule } from './jwt/jwt.module';
+import { DepositsModule } from './deposits/deposits.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, RedisModule, AppJwtModule],
+  imports: [AuthModule, PrismaModule, RedisModule, AppJwtModule, DepositsModule],
   controllers: [],
   providers: [],
 })
